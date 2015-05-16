5c4e41def7090baded23bf1e71b9d8ea834ecf4a
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-05-16 11:37:06 +0100

    adds tested API call to get the makers list

--- js/makerMixerController.js
+++ js/makerMixerController.js
@@ -1,3 +1,11 @@
-makerMixer.controller('MakerMixerController', [function() {
+makerMixer.controller('MakerMixerController', ['$http', function($http) {
+  var self = this;
+  self.list = [];
 
+  self.importList = function() {
+    $http.get('http://localhost:3000/makers').success(function() {
+    }).then(function(response) {
+      self.list = response.data;
+    }) 
+  }
 }]);
--- test/spec/makerMixerController.spec.js
+++ test/spec/makerMixerController.spec.js
@@ -8,7 +8,35 @@ describe('MakerMixerController', function() {
   }));
 
   it('initialises with an empty list', function() {
-    expect(ctrl.list).toBeUndefined();
+    expect(ctrl.list.length).toEqual(0);
   });
 
+  describe('when importing a list of names', function() {
+    
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "http://localhost:3000/makers")
+        .respond(
+        { items: list }
+      );
+   
+    }));
+
+    var list = [
+      {"_id":"55546fa1153f690b4082df55","name":"Nadav","pair":true,"blacklist":false,"numberOfCommits":100,"__v":0,"updated_at":"2015-05-14T09:49:21.243Z"},
+      {"_id":"55546fe7153f690b4082df56","name":"Kanishk","pair":true,"blacklist":false,"numberOfCommits":200,"__v":0,"updated_at":"2015-05-14T09:50:31.825Z"},
+      {"_id":"55547018153f690b4082df57","name":"Maciej","pair":true,"blacklist":false,"numberOfCommits":3000,"__v":0,"updated_at":"2015-05-14T09:51:20.488Z"}
+    ];
+
+    it('when import, lenght of list equal number of makers in database', function() { 
+      ctrl.importList();
+      httpBackend.flush();
+      expect(ctrl.list.items).toEqual(list);
+    });
+  });
 });
+
+
+
